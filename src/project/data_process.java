590384313a0f8820ffec158e51b533cd98133867
LnlyPineapple27 <datpt1000cyber@gmail.com>
2020-11-29 00:40:39 +0700

    ADD: Search by def + its console UI

--- src/project/data_process.java
+++ src/project/data_process.java
@@ -19,6 +19,7 @@ người dùng chọn.
 10. Chức năng đố vui, chương trình hiển thị 1 definition, với 4 slang words đáp án cho
 người dùng chọn.
  */
+/*
 public class data_process {
     public static final String INPUT_PATH = "input/slang.txt";
     public TreeMap<String, LinkedList<String>> data;
@@ -58,7 +59,8 @@ public class data_process {
         return this.data.get(word.trim());
     }
 
-
+    // 2. Chức năng tìm kiếm theo definition, hiển thị ra tất cả các slang words mà trong
+    // defintion có chứa keyword gõ vào.
 
 
     // ------------------------ Console UI things --------------------------
@@ -80,6 +82,105 @@ public class data_process {
         k.readfile(INPUT_PATH);
         // --------------------
         k.Console_searchSlang();
+    }
+
+}
+*/
+
+public class data_process {
+    public static final String INPUT_PATH = "input/slang.txt";
+    public TreeMap<String, Set<String>> data;
+
+    private void readfile(String dir){
+        this.data = new TreeMap<String, Set<String>>(String.CASE_INSENSITIVE_ORDER);
+
+        try {
+            File file = new File(dir);
+            FileReader fr = new FileReader(file);
+            BufferedReader br = new BufferedReader(fr);
+
+            String line_info;
+            while ((line_info = br.readLine()) != null){
+                String[] content = line_info.split("`"); // Parse a line to <Slang> and <Definitions>
+                if (content.length != 2)
+                    continue;
+                String[] syns = content[1].split("\\|"); // Parse <Definitions> if there are many of it
+                for (int i = 0; i < syns.length; i++)
+                    syns[i] = syns[i].trim();
+                // Set<Integer> targetSet = new HashSet<Integer>(Arrays.asList(sourceArray));
+
+                Set<String> words = new HashSet<String>(Arrays.asList(syns)); // Append processed data to treemap
+                (this.data).put(content[0], words);
+            }
+            fr.close();
+
+        }
+        catch (IOException err){
+            err.printStackTrace();
+        }
+    }
+    public void printdata(){
+        Set<Map.Entry<String, Set<String>>> entries = this.data.entrySet();
+        for(Map.Entry<String, Set<String>> entry : entries){
+            System.out.println( entry.getKey() + "\t->\t" + entry.getValue() );
+        }
+    }
+    //1. Chức năng tìm kiếm theo slang word.
+    public Set<String> searchSlang(String word){
+        return this.data.get(word.trim());
+    }
+
+    // 2. Chức năng tìm kiếm theo definition, hiển thị ra tất cả các slang words mà trong
+    // defintion có chứa keyword gõ vào.
+
+    public Set<String> searchDefinition(String word){
+        word = word.trim();
+        Set<String> result = new HashSet<String>();
+        Set<Map.Entry<String, Set<String>>> entries = this.data.entrySet();
+        for(Map.Entry<String,Set<String>> entry: entries){
+            Set<String> def = entry.getValue();
+            for (String i : def){
+                if (i.contains(word))
+                    result.add(entry.getKey());
+            }
+        }
+        return result;
+    }
+
+
+    // ------------------------ Console UI things --------------------------
+    public void Console_searchSlang(){
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter any word to search: ");
+        String kb = scan.nextLine();
+        scan.close();
+        Set<String> query = this.searchSlang(kb);
+        if (query == null)
+            System.out.println("No matches");
+        else
+            System.out.println("Definition: " + query.toString());
+    }
+
+    public void Console_searchDefinition(){
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter any definition word to search: ");
+        String kb = scan.nextLine();
+        scan.close();
+        Set<String> query = this.searchDefinition(kb);
+        if (query == null)
+            System.out.println("No matches");
+        else
+            System.out.println("Slang: " + query.toString());
+    }
+
+    // -----------------------------------------------------------------------
+    public static void main(String args[]){
+        data_process k = new data_process();
+        k.readfile(INPUT_PATH);
+        // --------------------
+        k.printdata();
+        //k.Console_searchSlang();
+        k.Console_searchDefinition();
     }
 
 }
